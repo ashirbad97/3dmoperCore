91623cf2dfc1d9e13764ffddffef1cb101fbff24
Rijul <rijul.soans@gmail.com>
2018-08-13 18:29:09 +0530

    Inital commit

---
+++ Assets/Scripts/LookAtObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class LookAtObject : MonoBehaviour {
+
+    Collider play_Collider;
+    public GameObject foveInterfaceGo;
+    public FoveInterface fi;
+    public GameObject progressBar;
+    
+	// Use this for initialization
+	void Start () {
+        play_Collider = GetComponent<Collider>();
+        progressBar.SetActive(true);
+        fi = foveInterfaceGo.GetComponent<FoveInterface>();
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int buildIndex = currentScene.buildIndex;
+        //if (FoveInterface.IsLookingAtCollider (this.gameObject.GetComponent<Collider>())) // raycasting: returns whether or not the user is looking at the collider
+        if ((fi.Gazecast(play_Collider)) && (play_Collider.name == "StartButton"))
+        {
+            progressBar.SetActive(true);
+           
+            SceneManager.LoadScene("Stimulus");
+        }
+        else if ((fi.Gazecast(play_Collider)) && (play_Collider.name == "QuitButton"))
+        {
+            progressBar.SetActive(true);
+            
+            Application.Quit();
+            Debug.Log("Application has been quit");
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/ReadXY.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class ReadXY : MonoBehaviour
+{
+	
+	public float[] posx;
+	public float[] posy;
+    public float[] posz; //New addition
+	
+	// Use this for initialization
+	void Awake () //Awake is used to initialize variables before game starts. Called only once
+    {
+        
+		ReadStringx();
+		ReadStringy();
+        ReadStringz();
+	}
+
+    void ReadStringx()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int buildIndex = currentScene.buildIndex;
+        string path = null;
+        switch (buildIndex)
+        {
+            case 1:
+                path = "Assets/Scripts/x_pos_fove.txt";
+                break;
+            case 2:
+                path = "Assets/Scripts/x_pos_fove1.txt";
+                break;
+        } 
+        //string path = "Assets/Scripts/x_pos_fove.txt";
+	
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path); 
+        
+        string[] numbers = reader.ReadToEnd().Split(','); //.Split('	');
+        posx = new float[numbers.Length]; //1400 in this case
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            posx[i] = float.Parse(numbers[i]);
+        }
+ 		reader.Close();
+
+        // The correct way to read file in Unity - else it wont load while building
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = Resources.Load("x_pos_fove") as TextAsset;
+    }
+	
+	 void ReadStringy()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int buildIndex = currentScene.buildIndex;
+        string path = null;
+        switch (buildIndex)
+        {
+            case 1:
+                path = "Assets/Scripts/y_pos_fove.txt";
+                break;
+            case 2:
+                path = "Assets/Scripts/y_pos_fove1.txt";
+                break;
+        }
+
+        //string path = "Assets/Scripts/y_pos_fove.txt";
+	
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path); 
+        
+        string[] numbers = reader.ReadToEnd().Split(','); //.Split('	');
+        posy = new float[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            posy[i] = float.Parse(numbers[i]);
+        }
+ 		reader.Close();
+
+        // The correct way to read file in Unity - else it wont load while building
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = Resources.Load("y_pos_fove") as TextAsset;
+    }
+
+    void ReadStringz()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        int buildIndex = currentScene.buildIndex;
+        string path = null;
+        switch (buildIndex)
+        {
+            case 1:
+                path = "Assets/Scripts/z_pos_fove.txt";
+                break;
+            case 2:
+                path = "Assets/Scripts/z_pos_fove1.txt";
+                break;
+        }
+
+
+
+        //string path = "Assets/Scripts/z_pos_fove.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+
+        string[] numbers = reader.ReadToEnd().Split(','); //.Split('	');
+        posz = new float[numbers.Length];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            posz[i] = float.Parse(numbers[i]);
+        }
+        reader.Close();
+
+        // The correct way to read file in Unity - else it wont load while building
+        AssetDatabase.ImportAsset(path);
+        TextAsset asset = Resources.Load("z_pos_fove") as TextAsset;
+    }
+
+
+
+}
